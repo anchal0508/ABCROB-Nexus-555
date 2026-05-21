4d9cb42d12f1392527ec7a765802b4fb64ad54c9
anchal.koshta007@gmail.com <genious@genious.genious>
2026-05-21 15:34:53 +0530

    header with 5 elements only with 5-6 projects planning

--- src/components/Home/Header.tsx
+++ src/components/Home/Header.tsx
@@ -3,14 +3,14 @@
 
 function Header() {
 
-    const items = ["Journey", "Impact", "Project", "Download Resume"];
+    const items = ["MySelf", "Certificates & Letters", "Classes", "Download Resume", "Switch Websites"];
 
     return (
         <header className="header-container">
             <h1 className="logo">
                 <span>ABCROB </span>
                 <span className="logo-555">Nexus-555</span>
-                </h1>
+            </h1>
             <ul className="list-header">
                 {items.map(item => <li className="header-list-items">{item}</li>)}
             </ul>
