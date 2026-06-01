5c80810300f8626e0166b3ac041bbc8292e04880
anchal.koshta007@gmail.com <genious@genious.genious>
2026-06-01 16:24:00 +0530

    checking saree website

--- src/sites/2-saree/SareeHome.tsx
+++ src/sites/2-saree/SareeHome.tsx
@@ -1,11 +1,16 @@
 // Storefront Front Page
 
+import Footer from "../1-portfolio/homePage/components/Footer";
+import Header from "../1-portfolio/homePage/components/Header";
+
 
 function SareeHome(){
 
     return (
         <>
+        <Header />
         <h1>Welcome to Saree Shop</h1>
+        <Footer />
         </>
     )
 }
